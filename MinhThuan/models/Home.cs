12008d197a7fe61b5c83bdc9e7f1a655dcf672b2
minh thuan <minhthuan@gmail.com>
2023-12-13 10:25:55 +0700

    Create Home.cs in models folder

---
+++ MinhThuan/models/Home.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhThuan.models
+{
+    internal class Home
+    {
+    }
+}
